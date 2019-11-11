a1b858ecfda80b8ae9e67f6424e2b49ae8ddb18d
bespalovad <55587308+bespalovad@users.noreply.github.com>
2019-11-12 00:12:09 +0300

    Create Lab2_Task2.py

---
+++ laboratory2/Lab2_Task2.py
@@ -0,0 +1,35 @@
+def cont ():
+	ans = input ()
+	if ans == "y":
+		main ()
+	elif ans != "n":
+		print ("Напишіть y якщо хочете продовжити роботу чи n якщо потрібно завершити роботу")
+		cont()
+	
+def read_natur ():
+	try:
+		a = int(input ())
+	except ValueError:
+		print ("Введіть натуральне число")
+		a = read_natur ()
+	if (a < 1):
+		print ("Введіть натуральне число")
+		a = read_natur ()
+	return a
+
+def main ():
+	print ("Введіть число")
+	n = str(read_natur ())
+	even_count = 0
+	n_len = len(n)
+	i = 0
+	while (i < n_len):
+		if (int(n[i]) % 2 == 0):
+			even_count += 1
+		i += 1
+	print ("Кількість парних цифр дорівнює", even_count)
+	print ("Продовжити? (y/n)")
+	cont ()
+	
+print ("Беспалов Антон Дмитрович \nЛабораторна робота №2 \nВаріант 3 \nЗнахождення кількості парних цифр числа")
+main()
